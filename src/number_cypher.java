30708b98ab91a3290f6754079dd24aca2b696edc
tLangelo <theislangelo@gmail.com>
2021-09-27 22:46:08 +0200

    encoder

--- src/number_cypher.java
+++ src/number_cypher.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class number_cypher {
+    public static Scanner scanner = new Scanner(System.in);
+    public static String alphabet = " abcdefghijklmnopqrstuvwxyzæøå";
+    //public static String numberAlphabet = "0123456789101112";
+
+    // Encoder
+    public static void numberCypherEncoder(String stringToEncode){
+        for (int i = 0; i < stringToEncode.length(); i++) {
+            if(i<stringToEncode.length()-1){
+                System.out.print(alphabet.indexOf((stringToEncode.charAt(i))) + ";");
+            }
+            else{
+                System.out.print(alphabet.indexOf(stringToEncode.charAt(i)));
+            }
+
+        }
+    }
+    // Decoder
+    public static void numberCypherDecoder(String stringToDecode){
+        for (int i = 0; i < stringToDecode.length(); i++) {
+            if(i==1){
+
+            }
+        }
+
+
+    }
+
     public static void main(String[] args) {
+        String userInput = scanner.nextLine();
+        numberCypherEncoder(userInput);
+
 
     }
 }
